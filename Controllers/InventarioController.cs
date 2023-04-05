d5d204fa280e1b1341f4e6eed452ae0c2ed50bfd
mariodev01 <mariovalentinomata@gmail.com>
2023-04-05 09:25:26 +0800

    usando storedProcedure

---
+++ Controllers/InventarioController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ManejoInventario.Controllers
+{
+    public class InventarioController : Controller
+    {
+        private readonly ManejoContext context;
+
+        public InventarioController(ManejoContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            var inventarios = context.verInventariosp().ToList();
+            return View(inventarios);
+        }
+    }
+}
--- ManejoContext.cs
+++ ManejoContext.cs
@@ -12,5 +12,16 @@ namespace ManejoInventario
 
         public DbSet<Producto> Productos { get; set; }
         public DbSet<Inventario> Inventarios { get; set; }
+
+
+        public List<Inventario> verInventariosp()
+        {
+            return Inventarios.FromSqlRaw("exec sp_ver_Inventario").ToList();
+        }
+
+        public void AñadirInventario(bool EnExistencia,int Cantidad,int ProductoId)
+        {
+            Database.ExecuteSqlRaw("exec sp_Añadir_Inventario {0},{1},{2}",EnExistencia, Cantidad, ProductoId);
+        }
     }
 }
